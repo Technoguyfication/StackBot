9584dcfc7930fcac7240163342ffb718b300889b
Hayden Andreyka <haydenandreyka@gmail.com>
2016-07-01 01:51:50 -0500

    Hotfix

--- src/StackBot/StackOverflow/StackManager.js
+++ src/StackBot/StackOverflow/StackManager.js
@@ -5,7 +5,7 @@ function getStackQuestion(id, callback) {
 	var filter = {
 		key: Config.ApiKeys.StackApps,
 		filter: '!-*f(6s6U8Q9b'	// body markdown and link
-	}
+	};
 	
 	
 }
@@ -17,6 +17,6 @@ function getStackQuestionID(url) {
 		return url.match(qIDRegex)[0];
 	} catch(er) {
 		logger.error('Failed to parse ID from stackoverflow link: %s', er.message);
-		throw;
+		throw er;
 	}
 }
