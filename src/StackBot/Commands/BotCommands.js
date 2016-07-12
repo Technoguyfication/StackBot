5ff732c5fe8b38d0fa4d2a7f50435f9802b0662f
Hayden Andreyka <haydenandreyka@gmail.com>
2016-07-12 16:36:10 -0500

    Removed stack list command from help section

--- src/StackBot/Commands/BotCommands.js
+++ src/StackBot/Commands/BotCommands.js
@@ -76,13 +76,11 @@ var commandList = {
 				Messages.Normal(msg.channel, util.format(
 					'Hello, I\'m StackBot, and here\'s my list of commands:\n\n' +
 					'`%s(Stack Overflow Query or Question ID)` to grab a question and answer from StackOverflow.\n' +
-					'`%s(Stack Overflow Query or Question ID)` does the same as `%s` but lists available questions and answers.\n\n' +
 					'**Append `%s` to front of the following commands:**\n' +
 					'%s\n\n' +
 					'*A webpage for contact information, support, and other details is coming soon.*',
 					
 					Config.Chat.StackCommand,
-					Config.Chat.StackListCommand,
 					Config.Chat.StackCommand,
 					Config.Chat.BotCommand,
 					helpBlock));
